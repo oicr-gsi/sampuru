62b9d0b19c51bb95d81cc3defc0b7ca8aebc3979
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-19 09:34:03 -0400

    Skeleton services

--- server/src/service/CaseService.java
+++ server/src/service/CaseService.java
@@ -1,4 +1,14 @@
 package service;
 
+import type.Case;
+
 public class CaseService extends Service {
+
+    public CaseService(){
+        super(Case.class);
+    }
+
+    public Case get(String name){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
