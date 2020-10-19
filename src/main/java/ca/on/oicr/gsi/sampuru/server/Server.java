19d91bfa0d554b12fdcdf095c154100f74221df6
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-19 14:57:26 -0400

    Server serves nothing but serves it well
    See if we can make the Service parameter handlers more generic later.
    Get server config from config file later.

--- src/main/java/ca/on/oicr/gsi/sampuru/server/Server.java
+++ src/main/java/ca/on/oicr/gsi/sampuru/server/Server.java
@@ -1,5 +1,22 @@
 package ca.on.oicr.gsi.sampuru.server;
 
-public class Server {
+import ca.on.oicr.gsi.sampuru.server.service.ProjectService;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.RoutingHandler;
 
+public class Server {
+    private static Undertow server;
+
+    private final static HttpHandler ROUTES = new RoutingHandler()
+            .get("/projects", ProjectService::getAllParams);
+
+    public static void main(String[] args){
+        server = Undertow.builder()
+                .addHttpListener(8088, "localhost") // TODO: get these from config file
+                .setHandler(ROUTES)
+                .build();
+        server.start();
+    }
 }
