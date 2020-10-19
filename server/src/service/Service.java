67964e368af4a25b11c2fac4f3aca6059bbb2f31
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-19 09:26:37 -0400

    Skeleton Service

--- server/src/service/Service.java
+++ server/src/service/Service.java
@@ -1,4 +1,24 @@
 package service;
 
-public class Service {
+import type.SampuruType;
+import java.util.List;
+
+public class Service<T extends SampuruType> {
+    Class<T> targetClass;
+
+    public Service(Class<T> newTarget){
+        targetClass = newTarget;
+    }
+
+    public T get(int id){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public List<T> getAll(){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public List<T> search(String term){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
