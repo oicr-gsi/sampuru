7f47fe71e359c523116a6d5221cffef2c7cffccd
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-19 09:34:03 -0400

    Skeleton services

--- server/src/service/QCableService.java
+++ server/src/service/QCableService.java
@@ -1,4 +1,14 @@
 package service;
 
+import type.QCable;
+
 public class QCableService extends Service {
+
+    public QCableService(){
+        super(QCable.class);
+    }
+
+    public QCable get(String alias){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
