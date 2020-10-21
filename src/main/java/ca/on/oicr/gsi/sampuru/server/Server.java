e1fcd37bf5ae3384813e9a39b69b87ea17a0aa21
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-21 14:26:34 -0400

    Maven setup with properties file

--- src/main/java/ca/on/oicr/gsi/sampuru/server/Server.java
+++ src/main/java/ca/on/oicr/gsi/sampuru/server/Server.java
@@ -1,16 +1,16 @@
 package ca.on.oicr.gsi.sampuru.server;
 
-import ca.on.oicr.gsi.sampuru.server.service.ProjectService;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.RoutingHandler;
+import io.undertow.util.Headers;
 
 public class Server {
     private static Undertow server;
 
     private final static HttpHandler ROUTES = new RoutingHandler()
-            .get("/projects", ProjectService::getAllParams);
+            .get("/", Server::helloWorld);
 
     public static void main(String[] args){
         server = Undertow.builder()
@@ -18,5 +18,10 @@ public class Server {
                 .setHandler(ROUTES)
                 .build();
         server.start();
+    }
+
+    private static void helloWorld(HttpServerExchange hse){
+        hse.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+        hse.getResponseSender().send("You found Sampuru!");
     }
 }
