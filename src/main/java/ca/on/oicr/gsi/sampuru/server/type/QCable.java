b47238ef8ff9794c96d53a21aaa4d4df913292e4
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-22 13:33:28 -0400

    Update QCable fields

--- src/main/java/ca/on/oicr/gsi/sampuru/server/type/QCable.java
+++ src/main/java/ca/on/oicr/gsi/sampuru/server/type/QCable.java
@@ -6,6 +6,7 @@ public class QCable extends SampuruType {
     public String OICRAlias;
     public String status;
     public String failureReason;
+    public String libraryDesign;
     public List<ChangelogEntry> changelog;
 
     public QCable(int id){
