acba514120bdaea833810b65f0ecdfc970d46241
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-22 11:13:43 -0400

    progress

--- src/main/java/ca/on/oicr/gsi/sampuru/server/DBConnector.java
+++ src/main/java/ca/on/oicr/gsi/sampuru/server/DBConnector.java
@@ -1,37 +1,29 @@
 package ca.on.oicr.gsi.sampuru.server;
 
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
-
-import static tables_generated.Tables.*;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
+// TODO: Later, let's not just have 1 connection, but a pool of them. Methods would probably be static.
 public class DBConnector {
-    Properties properties = readProperties();
-    String userName = properties.getProperty("dbUser");
-    String pw = properties.getProperty("dbPassword");
-    String url = properties.getProperty("dbUrl");
+    private Properties properties = readProperties();
+    private String userName = properties.getProperty("dbUser");
+    private String pw = properties.getProperty("dbPassword");
+    private String url = properties.getProperty("dbUrl");
+    private Connection connection;
 
-    public void connect(){
-        try(Connection conn = DriverManager.getConnection(url, userName, pw)){
-            DSLContext create = DSL.using(conn, SQLDialect.POSTGRES);
-            Result<Record> result = create.select().from(PROJECT).fetch();
-
-            for (Record r: result){
-                Integer id = r.getValue(PROJECT.ID);
-                System.out.println("ID: " + id);
-            }
+    public DBConnector(){
+        try {
+            connection = DriverManager.getConnection(url, userName, pw);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
+
+    public Connection getConnection(){
+        return connection;
     }
 
     private Properties readProperties() {
