c636e2f460c881bbada20eefcf316ac912c1897a
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-26 18:29:04 -0400

    getAll works, i think!

--- src/main/java/ca/on/oicr/gsi/sampuru/server/service/Service.java
+++ src/main/java/ca/on/oicr/gsi/sampuru/server/service/Service.java
@@ -1,15 +1,8 @@
 package ca.on.oicr.gsi.sampuru.server.service;
 
-import static tables_generated.Tables.*;
-
-import ca.on.oicr.gsi.sampuru.server.DBConnector;
 import ca.on.oicr.gsi.sampuru.server.type.SampuruType;
-import org.jooq.DSLContext;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
 
-import java.sql.Connection;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class Service<T extends SampuruType> {
@@ -19,20 +12,12 @@ public class Service<T extends SampuruType> {
         targetClass = newTarget;
     }
 
-    public T get(int id){
-        throw new UnsupportedOperationException("Not implemented yet");
+    public T get(int id) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        return targetClass.getDeclaredConstructor(Integer.class).newInstance(id);
     }
 
-    public List<T> getAll(){
-        //throw new UnsupportedOperationException("Not implemented yet");
-
-        Connection connection = new DBConnector().getConnection();
-        DSLContext query = DSL.using(connection, SQLDialect.POSTGRES);
-        try {
-            Result<Record> result = query.select().from().fetch();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
+    public List<T> getAll() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return (List<T>)targetClass.getDeclaredMethod("getAll").invoke(null);
     }
 
     public List<T> search(String term){
