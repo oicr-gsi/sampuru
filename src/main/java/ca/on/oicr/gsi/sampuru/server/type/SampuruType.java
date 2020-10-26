4e1999e5bebd438f16f04e88a11fab808c8ea68c
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-26 18:29:04 -0400

    getAll works, i think!

--- src/main/java/ca/on/oicr/gsi/sampuru/server/type/SampuruType.java
+++ src/main/java/ca/on/oicr/gsi/sampuru/server/type/SampuruType.java
@@ -1,18 +1,5 @@
 package ca.on.oicr.gsi.sampuru.server.type;
 
-import org.jooq.Table;
-
-import java.util.List;
-
-public abstract class SampuruType<T> {
-    public static Table TABLE_NAME;
+public abstract class SampuruType {
     public int id;
-
-    public static List<? extends SampuruType> getAll(){
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    public static List<? extends SampuruType> search(String term){
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
 }
