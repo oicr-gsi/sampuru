3b31f8f396cec31a54a07ade32fa2c7b395e08a0
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-10-29 16:49:21 -0400

    Add parent id

--- src/main/java/ca/on/oicr/gsi/sampuru/server/type/QCable.java
+++ src/main/java/ca/on/oicr/gsi/sampuru/server/type/QCable.java
@@ -8,12 +8,14 @@ import java.util.List;
 
 import static tables_generated.Tables.*;
 
+
 public class QCable extends SampuruType {
     public String OICRAlias;
     public String status;
     public String failureReason;
     public String libraryDesign;
     public String type;
+    public int parentId;
     public List<ChangelogEntry> changelog;
 
     public QCable(int newId) throws Exception {
@@ -37,6 +39,7 @@ public class QCable extends SampuruType {
         failureReason = dbRecord.getValue(QCABLE.FAILURE_REASON);
         libraryDesign = dbRecord.getValue(QCABLE.LIBRARY_DESIGN);
         type = dbRecord.getValue(QCABLE.QCABLE_TYPE);
+        parentId = dbRecord.getValue(QCABLE.PARENT_ID);
 
         changelog = dbConnector.getMany(CHANGELOG.ID, CHANGELOG.QCABLE_ID, id, ChangelogEntry.class);
     }
