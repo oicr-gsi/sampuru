71b48f3afc939c793cdd34e40d4b70ebff852c88
Angie Mosquera <angie.mosquera@oicr.on.ca>
2021-02-16 15:42:32 -0500

    [GP-2581] Make QCables table look like wireframes (#63)
    * Make QCables table look like wireframes
    
    * Make things look prettier
    
    * Add case column to QCables table
    
    * Don't register click events when user is selecting text to copy

--- sampuru-ui/src/qcables.ts
+++ sampuru-ui/src/qcables.ts
@@ -5,7 +5,7 @@
 import {
   bootstrapTable,
   busyDialog,
-  ComplexElement,
+  ComplexElement, elementFromTag,
   navbar,
   tableBodyFromRows,
   tableRow
@@ -42,7 +42,7 @@ function statusToClassName(status: string | null) {
 
 export function qcablesTable(qcables: QCable[], projectName: string): void {
   const pageContainer = document.createElement("div");
-  const pageHeader = document.createElement("h2");
+  const pageHeader = document.createElement("h3");
   pageHeader.innerText = "QCables (" + projectName + ")";
 
   //todo: click on a cell and show alias??
@@ -51,31 +51,35 @@ export function qcablesTable(qcables: QCable[], projectName: string): void {
     .forEach((qcable) => {
       tableRows.push(tableRow(null,
         {
-          contents: qcable.tissue_qcable_alias,
+          contents: qcable.case_id,
+          className: "qcable-table-donor-case"
+        },
+        {
+          contents: qcable.tissue_qcable_alias ? qcable.tissue_qcable_alias: "",
           className: statusToClassName(qcable.tissue_qcable_status)
         },
         {
-          contents: qcable.extraction_qcable_alias,
+          contents: qcable.extraction_qcable_alias ? qcable.extraction_qcable_alias: "",
           className: statusToClassName(qcable.extraction_qcable_status)
         },
         {
-          contents: qcable.library_preparation_qcable_alias,
+          contents: qcable.library_preparation_qcable_alias ? qcable.library_preparation_qcable_alias: "",
           className: statusToClassName(qcable.library_preparation_qcable_status)
         },
         {
-          contents: qcable.low_pass_sequencing_qcable_alias,
+          contents: qcable.low_pass_sequencing_qcable_alias ? qcable.low_pass_sequencing_qcable_alias: "",
           className: statusToClassName(qcable.low_pass_sequencing_qcable_status)
         },
         {
-          contents: qcable.full_depth_sequencing_qcable_alias,
+          contents: qcable.full_depth_sequencing_qcable_alias ? qcable.full_depth_sequencing_qcable_alias: "",
           className: statusToClassName(qcable.full_depth_sequencing_qcable_status)
         },
         {
-          contents: qcable.informatics_interpretation_qcable_alias,
+          contents: qcable.informatics_interpretation_qcable_alias ? qcable.informatics_interpretation_qcable_alias: "",
           className: statusToClassName(qcable.informatics_interpretation_qcable_status)
         },
         {
-          contents: qcable.final_report_qcable_alias,
+          contents: qcable.final_report_qcable_alias ? qcable.final_report_qcable_alias: "",
           className: statusToClassName(qcable.final_report_qcable_status)
         }
       ));
@@ -83,6 +87,7 @@ export function qcablesTable(qcables: QCable[], projectName: string): void {
     });
 
   const tableHeaders = new Map([
+    ["case_id", "Case"],
     ["tissue_qcable_alias", "Receipt/Inspection"],
     ["extraction_qcable_alias", "Extraction"],
     ["library_preparation_qcable_alias", "Library Preparation"],
@@ -106,6 +111,33 @@ export function qcablesTable(qcables: QCable[], projectName: string): void {
         return 'Search QCables';
       }
     });
+  });
+
+  $('#table').on('click-cell.bs.table', function(event, field, value, row, $element) {
+    const cellValue = elementFromTag("div", "card",
+      elementFromTag("div", "card-body", value));
+
+    const selection = window.getSelection();
+    const isEmpty = (selection != null) ? selection.toString() : "";
+    // Don't register click events when user is selecting text
+    if(isEmpty.length <= 0) {
+      const childNodes = $element.children();
+
+      if (childNodes.length) {
+        childNodes.remove();
+      } else {
+        if(value != "" && field != "case_id") {
+          $element.attr('id', value).append(cellValue.element);
+        } else if (field != "case_id") {
+          // Let user know QCable hasn't been created yet
+          const emptyNotifier = elementFromTag("div", "card",
+            elementFromTag("div", "card-body", "Hasn't yet started"));
+          $element.attr('id', field).append(emptyNotifier.element);
+        }
+      }
+    }
+
+
   });
 }
 
