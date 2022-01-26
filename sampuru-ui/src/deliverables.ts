1e99c089779670122690e8160c1a025b6440fe55
Isa Mohideen <isa.mohideen@gmail.com>
2022-01-26 16:30:02 -0500

    Added fields for case-ID, notes, and expiry date

--- sampuru-ui/src/deliverables.ts
+++ sampuru-ui/src/deliverables.ts
@@ -8,10 +8,37 @@ document.body.appendChild(header);
 const form = document.createElement("form");
 
 const projectNameTextBox = createSingleLineLabeledInput("Project Name: ", "text", "projectNameTextBox", "form-control");
+projectNameTextBox.style.padding = "15px";
 form.appendChild(projectNameTextBox);
 
-const deliverableInfoTextBox = createMultiLineLabeledInput("Deliverable Information: ", "5", "deliverableInfoTextBox", "form-control");
-form.appendChild(deliverableInfoTextBox);
+const deliverableLocationTextBox = createMultiLineLabeledInput("Deliverable Location: ", "3", "deliverableLocationTextBox", "form-control");
+deliverableLocationTextBox.style.padding = "15px";
+form.appendChild(deliverableLocationTextBox);
+
+const caseIDTextBox = createMultiLineLabeledInput("Case ID: ", "3", "caseIDTextBox", "form-control");
+caseIDTextBox.style.padding = "15px";
+form.appendChild(caseIDTextBox);
+
+const notesTextBox = createSingleLineLabeledInput("Notes: ", "text", "notesTextBox", "form-control");
+notesTextBox.style.padding = "15px";
+form.appendChild(notesTextBox);
+
+const formGroup = document.createElement("div");
+formGroup.classList.add("form-group");
+
+const expiryDateLabel = document.createElement("label");
+expiryDateLabel.innerText = "Expiry Date: ";
+formGroup.appendChild(expiryDateLabel);
+
+const lineBreak = document.createElement("br");
+formGroup.appendChild(lineBreak);
+
+const expiryDateTextBox = document.createElement("input");
+expiryDateTextBox.setAttribute("type", "datetime-local");
+expiryDateTextBox.setAttribute("id", "expiryDateTextBox");
+formGroup.appendChild(expiryDateTextBox);
+formGroup.style.padding = "15px";
+form.appendChild(formGroup);
 
 const submitButton = document.createElement("button");
 submitButton.className = "btn btn-primary";
